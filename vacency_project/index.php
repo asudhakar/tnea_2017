4464eefc006642164e538eb551dad5fdcfccda7c
Sudhakar Annadurai <asudhakar@live.in>
2017-07-28 21:24:03 +0530

    Add files via upload

---
+++ vacency_project/index.php
@@ -0,0 +1,42 @@
+	<iframe src='http://139.59.40.213/vacency_project/shanmugha.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/avs1.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/avs2.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/dhirajlal.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/erode_sengunthar.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/excel1.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/excel2.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/irtt.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/kavery.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/knowledge.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/ksr1.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/ksr2.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/ksr3.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/mahendra.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/mahendra1.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/mahendra2.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/mahendra3.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/mahendra4.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/muthayammal1.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/muthayammal2.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/narasu.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/paavai.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/paavai1.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/paavai2.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/sengunthar1.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/sengunthar2.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/sona.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/vidyaavikaas.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/vivekanandha1.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/vivekanandha2.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/annapoorna.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/NAGARATHINAM.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/CHETTINAD.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/ganesh.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/GNANAMANI.php'>  <p>Your browser does not support iframes.</p></iframe>
+	<iframe src='http://139.59.40.213/vacency_project/king.php'>  <p>Your browser does not support iframes.</p></iframe>
+	
+	<script language="javascript">
+		setTimeout(function(){
+		   window.location.reload(1);
+		}, 30000);
+	</script>
